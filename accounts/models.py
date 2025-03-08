d2c414161f0b73ccbc30d761f9444ff84d942167
AhmedMahmoud <ahmedmahmoudtahtawy@gmail.com>
2025-03-08 19:35:02 +0200

    add create userprofile signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -9,4 +10,11 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
-    
+
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])  # instance is the user object that is created
+post_save.connect(create_profile, sender=User)      
+# This code will create a user profile whenever a new user is created.
+# The create_profile function is connected to the User model's post_save signal.        
